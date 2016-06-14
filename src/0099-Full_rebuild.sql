c06f4bf3aeaf541ec2f46d54aa27cdcd8757fd73
Juan Pedro Pérez <jp.alcantara@gstgis.com>
2016-06-14 12:54:00 +0200

    WIP

--- src/0099-Full_rebuild.sql
+++ src/0099-Full_rebuild.sql
@@ -1,6 +1,8 @@
 -- Full database recovery
 
+\i 0000-Database-Drop.sql
 \i 0000-Database-DDL.sql
+\i 0020-Error-Drop.sql
 \i 0010-Schema-DDL.sql
 \i 0020-Error-DDL.sql
 \i 0030-Functions.sql
---
+++ src/0099-Soft_rebuild.sql
@@ -0,0 +1,8 @@
+-- Full database recovery
+
+\i 0010-Schema-Drop.sql
+\i 0020-Error-Drop.sql
+\i 0010-Schema-DDL.sql
+\i 0020-Error-DDL.sql
+\i 0030-Functions.sql
+\i 0090-Import_data.sql
